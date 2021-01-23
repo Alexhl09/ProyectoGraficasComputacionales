eb40b5cbc7dcb76c087825bd46c100e67c987def
Alejandro Hernández López <alexhl1999@hotmail.com>
2021-01-22 19:16:05 -0600

    Agregado exterior

--- public/client.js
+++ public/client.js
@@ -1,4 +1,6 @@
 import * as THREE from '/build/three.module.js';
+import { RectAreaLightHelper } from '/jsm/helpers/RectAreaLightHelper.js';
+import { RectAreaLightUniformsLib } from '/jsm/lights/RectAreaLightUniformsLib.js';
 import {OrbitControls} from '/jsm/controls/OrbitControls.js';
 import Stats from '/jsm/libs/stats.module.js';
 
@@ -15,7 +17,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
+RectAreaLightUniformsLib.init();
 const controls = new OrbitControls(camera, renderer.domElement);
 
 window.addEventListener('resize', () => {
@@ -40,6 +42,8 @@ const cube = new THREE.Mesh(geometry, material);
 cube.receiveShadow = true;
 scene.add(cube);
 
+const exterior = new THREE.Object3D();
+
 
 // Piso
 
@@ -50,7 +54,7 @@ const floorMaterial = new THREE.MeshBasicMaterial( {color: 0xff69b4, side: THREE
 const floor = new THREE.Mesh( floorGeometry, floorMaterial );
 floor.rotation.x = Math.PI / 2;
 floor.position.y = 0;
-scene.add( floor );
+exterior.add( floor );
 
 
 // Techo
@@ -62,7 +66,7 @@ const ceilingMaterial = new THREE.MeshBasicMaterial( {color: 0xff69b4, side: THR
 const ceiling = new THREE.Mesh( ceilingGeometry, ceilingMaterial );
 ceiling.rotation.x = Math.PI / 2;
 ceiling.position.y = 20;
-scene.add( ceiling );
+exterior.add( ceiling );
 
 
 
@@ -70,7 +74,7 @@ scene.add( ceiling );
 
 var walls = []
 
-for(var i = 0; i<4; i++){
+for(var i = 0; i<3; i++){
 const wallGeometry = new THREE.PlaneGeometry( 20, 100, 32 );
 wallGeometry.widthSegments = 2;
 wallGeometry.heightSegments = 2;
@@ -94,21 +98,15 @@ switch(i){
         wall.rotation.y = Math.PI ;
         wall.position.z = 50;
         break;
-    case 3:
-        wall.rotation.x = 0;
-        wall.rotation.z = Math.PI/2;
-        wall.rotation.y = Math.PI ;
-        wall.position.z = -50;
-        break;
 }
 walls[i] = wall;
-scene.add( walls[i] );
+exterior.add( walls[i] );
 }
 
 // LUZ
 var spot1 = new THREE.SpotLight(0xfafafa);
 spot1.position.set(20, 50, 30);
-scene.add(spot1);
+exterior.add(spot1);
 
 var animate = function () {
     requestAnimationFrame(animate);
@@ -120,6 +118,110 @@ var animate = function () {
 };
 
 
+// Ventanas izquierdas
+
+var windowsLeft = []
+
+for(var i = 0; i<2; i++){
+const windowGeometry = new THREE.PlaneGeometry( 20, 20, 32 );
+windowGeometry.widthSegments = 2;
+windowGeometry.heightSegments = 2;
+const windowGMaterial = new THREE.MeshBasicMaterial( {color: 0xb569ff, side: THREE.DoubleSide, wireframe: true} );
+const windowObject = new THREE.Mesh( windowGeometry, windowGMaterial );
+windowObject.rotation.x = Math.PI;
+windowObject.rotation.y = Math.PI;
+windowObject.rotation.z = Math.PI / 2;
+windowObject.position.y = 10;
+windowObject.position.x = -40 + (20 * i);
+windowObject.position.z = -50;
+windowsLeft[i] = windowObject;
+exterior.add( windowsLeft[i] );
+}
+
+
+// Ventanas izquierdas
+
+var windowsRight = []
+
+for(var i = 0; i<2; i++){
+const windowGeometry = new THREE.PlaneGeometry( 20, 20, 32 );
+windowGeometry.widthSegments = 2;
+windowGeometry.heightSegments = 2;
+const windowGMaterial = new THREE.MeshBasicMaterial( {color: 0xb569ff, side: THREE.DoubleSide, wireframe: true} );
+const windowObject = new THREE.Mesh( windowGeometry, windowGMaterial );
+windowObject.rotation.x = Math.PI;
+windowObject.rotation.y = Math.PI;
+windowObject.rotation.z = Math.PI / 2;
+windowObject.position.y = 10;
+windowObject.position.x = 40 - (20 * i);
+windowObject.position.z = -50;
+windowsRight[i] = windowObject;
+exterior.add( windowsRight[i] );
+}
+
+// Ventanas arriba donde esta logo
+
+const windowUpGeometry = new THREE.PlaneGeometry( 8, 20, 10 );
+windowUpGeometry.widthSegments = 2;
+windowUpGeometry.heightSegments = 2;
+const windowUpGaterial = new THREE.MeshBasicMaterial( {color: 0xb569ff, side: THREE.DoubleSide, wireframe: true} );
+const windowUpObject = new THREE.Mesh( windowUpGeometry, windowUpGaterial );
+windowUpObject.rotation.x = Math.PI;
+windowUpObject.rotation.y = Math.PI;
+windowUpObject.rotation.z = Math.PI / 2;
+windowUpObject.position.y = 16;
+windowUpObject.position.x = 0;
+windowUpObject.position.z = -50;
+exterior.add( windowUpObject);
+
+// Pantallas izquierdas
+
+var screens = []
+
+for(var i = 0; i<2; i++){
+const screenGeometry = new THREE.PlaneGeometry( 80, 8, 48 );
+screenGeometry.widthSegments = 2;
+screenGeometry.heightSegments = 2;
+const screenMaterial = new THREE.MeshBasicMaterial( {color: 0xbaffaa, side: THREE.DoubleSide, wireframe: true} );
+const screenObject = new THREE.Mesh( screenGeometry, screenMaterial );
+screenObject.rotation.x = Math.PI;
+screenObject.rotation.y = Math.PI/2;
+screenObject.position.y = 10;
+screenObject.position.x = -50 + (i*100);
+screenObject.position.z = 0;
+screens[i] = screenObject;
+exterior.add( screens[i] );
+}
+
+
+
+
+// Lamparas techo
+
+var lamparas = []
+for(var i = 0; i< 5; i++){
+for(var j = 0;j <5;j++){
+    const width = 10;
+const height = 10;
+const intensity = 1;
+const rectLight = new THREE.RectAreaLight( 0xffffff, intensity,  width, height );
+rectLight.position.set( 30, 20, -40 );
+rectLight.rotation.x = Math.PI/2;
+rectLight.position.z += j * 20;
+rectLight.position.x -= i * 20;
+rectLight.rotation.y = Math.PI;
+lamparas[i] = rectLight;
+exterior.add( lamparas[i] )
+
+const rectLightHelper = new RectAreaLightHelper( rectLight );
+rectLight.add( rectLightHelper );
+}
+}
+
+
+
+
+scene.add(exterior);
 function render() {
     renderer.render(scene, camera);
 }
